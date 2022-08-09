8451fc3136666b81bd05be18edfd4c2af35ef3e1
robert-argolo <robert.s.argolo@gmail.com>
2022-08-09 11:43:31 -0300

    componente de botão criado

--- literacy-educational-game/src/App.js
+++ literacy-educational-game/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from './components/Buttons/index';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Devs Education</h1>
+      <Button/>
     </div>
   );
 }
