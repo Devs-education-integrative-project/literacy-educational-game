0bf3d1e89e662796754a8afc0f8188c2432fcfec
Robert Argolo <robert.s.argolo@gmail.com>
2022-08-09 18:37:28 -0300

    modais prontos, falta colocar fixed

--- literacy-educational-game/src/App.js
+++ literacy-educational-game/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import ButtonsStartGame from './components/Buttons/BtnsStartGame';
+import ModalAcerto from './components/ModalAcertoErro/ModalAcerto';
+import ModalErro from './components/ModalAcertoErro/ModalErro';
 
 const App = () => {
   return (
     <div className="App">
       <h1>img qual é a letra</h1>
       <ButtonsStartGame/>
+      <ModalAcerto/>
+      <ModalErro/>
     </div>
   );
 }
