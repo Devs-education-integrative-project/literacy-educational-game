e340e07f2c3f89428a51ad511568151e5b3f7352
Luiz-de-Lima <luiz12359@gmail.com>
2022-08-11 00:53:43 -0300

    tela opção de iniciar um novo jogo feita

--- literacy-educational-game/src/App.jsx
+++ literacy-educational-game/src/App.jsx
@@ -1,16 +1,17 @@
-
-import './App.css';
-import ButtonsStartGame from './components/Buttons/BtnsStartGame';
+import Button from './components/Buttons/BtnsStartGame';
 import ModalAcerto from './components/ModalAcertoErro/ModalAcerto';
 import ModalErro from './components/ModalAcertoErro/ModalErro';
+import './App.css';
+import TelaDeOpcao from './components/TelaDeOpcao/TelaDeOpcao';
+
 
 const App = () => {
   return (
     <div className="App">
       <h1>img qual é a letra</h1>
-      <ButtonsStartGame/>
-      <ModalAcerto/>
-      <ModalErro/>
+      <Button>Novo Jogo</Button>
+      <Button>Continuar</Button>
+      <TelaDeOpcao />
     </div>
   );
 }
