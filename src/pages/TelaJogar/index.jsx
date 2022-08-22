b3eae1a02e35ccfcb3a4aad956bb0ea31177b911
Robert Argolo <robert.s.argolo@gmail.com>
2022-08-22 12:44:39 -0300

    desabilita os botões apos responder

--- src/pages/TelaJogar/index.jsx
+++ src/pages/TelaJogar/index.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import './index.css'
 import api from '../../assets/APIs/quiz.json'
-import { getActiveElement, getValue } from '@testing-library/user-event/dist/utils';
-import ModalAcerto from '../../components/ModalAcerto';
 
-let numberQuiz = 0;
+
+let numberQuiz =0;
 const TelaJogar = () => {
     
+  const [disable, setDisable] = React.useState(false);
     
     function handleClick(e){
 
       if(e.target.innerText===api[numberQuiz].answer){
         console.log('acertou');
         e.target.className="btn-green";
+        setDisable(true)
+        
         
       }else{
         console.log('errou');
         e.target.className="btn-red"
+        setDisable(true)
     }
 
 
@@ -26,11 +29,11 @@ const TelaJogar = () => {
       <div className='jogar'>
       <p className='pergunta'>{api[numberQuiz].question}</p>
       <img src={api[numberQuiz].imagem} alt={api[numberQuiz].alt} className="image-game" />
-      <div className='jogar-jogo__btn' >
-        <button className="btn-white" key={1} onClick={handleClick} >{api[numberQuiz].btns[0]}</button>
-        <button className="btn-white" key={2} onClick={handleClick} >{api[numberQuiz].btns[1]}</button>
-        <button className="btn-white" key={3} onClick={handleClick} >{api[numberQuiz].btns[2]}</button>
-        <button className="btn-white" key={4} onClick={handleClick} >{api[numberQuiz].btns[3]}</button>
+      <div className='jogar-jogo__btn'>
+        <button disabled={disable} className="btn-white" key={1} onClick={handleClick} >{api[numberQuiz].btns[0]}</button>
+        <button disabled={disable} className="btn-white" key={2} onClick={handleClick} >{api[numberQuiz].btns[1]}</button>
+        <button disabled={disable} className="btn-white" key={3} onClick={handleClick} >{api[numberQuiz].btns[2]}</button>
+        <button disabled={disable} className="btn-white" key={4} onClick={handleClick} >{api[numberQuiz].btns[3]}</button>
       </div>
       </div>
     </div>
