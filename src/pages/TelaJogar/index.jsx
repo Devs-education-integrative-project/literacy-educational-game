7727495c891a582699b5f086c4cf488c9b809fd7
Robert Argolo <robert.s.argolo@gmail.com>
2022-08-22 12:22:14 -0300

    mudar a cor dos botões ao acertar ou errar o quiz

--- src/pages/TelaJogar/index.jsx
+++ src/pages/TelaJogar/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import './index.css'
 import api from '../../assets/APIs/quiz.json'
+import { getActiveElement, getValue } from '@testing-library/user-event/dist/utils';
+import ModalAcerto from '../../components/ModalAcerto';
 
 let numberQuiz = 0;
 const TelaJogar = () => {
-  function handleClick(e){
-    if(e===api[numberQuiz].answer){
-      console.log('acertou');
+    
+    
+    function handleClick(e){
 
-    }else{
-      console.log('errou');
+      if(e.target.innerText===api[numberQuiz].answer){
+        console.log('acertou');
+        e.target.className="btn-green";
+        
+      }else{
+        console.log('errou');
+        e.target.className="btn-red"
     }
+
+
   }
   return (
     <div className="container">
@@ -18,10 +27,10 @@ const TelaJogar = () => {
       <p className='pergunta'>{api[numberQuiz].question}</p>
       <img src={api[numberQuiz].imagem} alt={api[numberQuiz].alt} className="image-game" />
       <div className='jogar-jogo__btn' >
-        <button className='btns-quiz' key={1} onClick={()=>handleClick(api[numberQuiz].btns[0])} >{api[numberQuiz].btns[0]}</button>
-        <button className='btns-quiz' key={2} onClick={()=>handleClick(api[numberQuiz].btns[1])} >{api[numberQuiz].btns[1]}</button>
-        <button className='btns-quiz' key={3} onClick={()=>handleClick(api[numberQuiz].btns[2])} >{api[numberQuiz].btns[2]}</button>
-        <button className='btns-quiz' key={4} onClick={()=>handleClick(api[numberQuiz].btns[3])} >{api[numberQuiz].btns[3]}</button>
+        <button className="btn-white" key={1} onClick={handleClick} >{api[numberQuiz].btns[0]}</button>
+        <button className="btn-white" key={2} onClick={handleClick} >{api[numberQuiz].btns[1]}</button>
+        <button className="btn-white" key={3} onClick={handleClick} >{api[numberQuiz].btns[2]}</button>
+        <button className="btn-white" key={4} onClick={handleClick} >{api[numberQuiz].btns[3]}</button>
       </div>
       </div>
     </div>
