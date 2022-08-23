2765bce8f0a724f7904326b0493e119f06e4d6be
Luiz-de-Lima <luiz12359@gmail.com>
2022-08-23 11:14:47 -0300

    função que muda para o proximo quiz

--- src/pages/TelaJogar/index.jsx
+++ src/pages/TelaJogar/index.jsx
@@ -1,42 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 import api from '../../assets/APIs/quiz.json'
 
+let numberQuiz = 0;
 
-let numberQuiz =0;
 const TelaJogar = () => {
-    
+
   const [disable, setDisable] = React.useState(false);
-    
-    function handleClick(e){
+  const [quiz, setQuiz] = useState([])
+  const [correct, setCorrect] = useState()
+  console.log()
+  useEffect(() => {
+    setQuiz(api)
+  }, [])
 
-      if(e.target.innerText===api[numberQuiz].answer){
-        console.log('acertou');
-        e.target.className="btn-green";
-        setDisable(true)
-        
-        
-      }else{
-        console.log('errou');
-        e.target.className="btn-red"
-        setDisable(true)
+  function handleClick(e) {
+    if (e.target.innerText === quiz[numberQuiz].answer) {
+      console.log('acertou');
+      e.target.classList.add("btn-green");
+
+      nextQuestion()
+    } else {
+      console.log('errou');
+      e.target.classList.add("btn-white");
+
+      nextQuestion()
+
     }
+  }
 
+  function nextQuestion() {
+    if (numberQuiz <= 26) {
+
+      setCorrect(numberQuiz++)
+    }
 
   }
   return (
     <div className="container">
+
       <div className='jogar'>
-      <p className='pergunta'>{api[numberQuiz].question}</p>
-      <img src={api[numberQuiz].imagem} alt={api[numberQuiz].alt} className="image-game" />
-      <div className='jogar-jogo__btn'>
-        <button disabled={disable} className="btn-white" key={1} onClick={handleClick} >{api[numberQuiz].btns[0]}</button>
-        <button disabled={disable} className="btn-white" key={2} onClick={handleClick} >{api[numberQuiz].btns[1]}</button>
-        <button disabled={disable} className="btn-white" key={3} onClick={handleClick} >{api[numberQuiz].btns[2]}</button>
-        <button disabled={disable} className="btn-white" key={4} onClick={handleClick} >{api[numberQuiz].btns[3]}</button>
+
+        <p className='pergunta'>{api[numberQuiz].question}</p>
+        <img src={api[numberQuiz].imagem} alt={api[numberQuiz].alt} className="image-game" />
+        <div className='jogar-jogo__btn'>
+          <button disabled={disable} className="btn-white" key={1} onClick={handleClick} >{api[numberQuiz].btns[0]}</button>
+          <button disabled={disable} className="btn-white" key={2} onClick={handleClick} >{api[numberQuiz].btns[1]}</button>
+          <button disabled={disable} className="btn-white" key={3} onClick={handleClick} >{api[numberQuiz].btns[2]}</button>
+
+          <button disabled={disable} className="btn-white" key={4} onClick={handleClick} >{api[numberQuiz].btns[3]}</button>
+        </div>
       </div>
-      </div>
-    </div>
+
+    </div >
   )
 }
 
