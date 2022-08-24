a5970eb635605e9a428ba7aca4aa3b8ee95073ef
Robert Argolo <robert.s.argolo@gmail.com>
2022-08-24 20:26:57 -0300

    mostrar modal e contar pontuação add

--- src/pages/TelaJogar/index.jsx
+++ src/pages/TelaJogar/index.jsx
@@ -1,68 +1,109 @@
-import React, { useEffect, useState } from 'react'
-import './index.css'
-import api from '../../assets/APIs/quiz.json'
+import React, { useEffect, useState } from 'react';
+import './index.css';
+import api from '../../assets/APIs/quiz.json';
+import ModalAcerto from '../../components/ModalAcerto';
+import ModalErro from '../../components/ModalErro';
 
-let numberQuiz = 0;
+// let numberQuiz = 0;
 
 const TelaJogar = () => {
+  const [disableButtons, setDisableButtons] = React.useState(false);
+  const [numberQuiz, setNumberQuiz] = React.useState(0);
+  const [modalAcerto, setModalAcerto] = React.useState('hiddenModalAcerto');
+  const [modalErro, setModalErro] = React.useState('hiddenModalErro');
+  const [score, setScore] = React.useState(100);
+  // console.log(score);
 
-  const [disable, setDisable] = React.useState(false);
-  const [quiz, setQuiz] = useState([])
-  const [correct, setCorrect] = useState()
-  console.log()
-  useEffect(() => {
-    setQuiz(api)
-  }, [])
+  
+  // useEffect(() => {
+  //   setQuiz(api);
+  // }, []);
 
   function handleClick(e) {
-    if (e.target.innerText === quiz[numberQuiz].answer) {
-      console.log('acertou');
-      e.target.classList.add("btn-green");
-      setDisable(true);
-setTimeout(() => {
-  nextQuestion(e)
-}, 1000);
-
-    } else {
-      console.log('errou');
-      e.target.classList.add("btn-red");
-      setDisable(true);
+    if (e.target.innerText === api[numberQuiz].answer) {
+      // console.log('acertou');
+      e.target.classList.add('btn-green');
+      setModalAcerto('');
+      setDisableButtons(true);
       setTimeout(() => {
-  nextQuestion(e)
-}, 1000);
-
+        nextQuestion(e);
+      }, 2000);
+    } else {
+      // console.log('errou');
+      e.target.classList.add('btn-red');
+      setModalErro('');
+      setDisableButtons(true);
+      setScore(score - 4)
+      setTimeout(() => {
+        nextQuestion(e);
+      }, 2000);
     }
   }
 
   function nextQuestion(e) {
     if (numberQuiz <= 26) {
-      console.log(e.target);
-     e.target.classList.remove("btn-green");
-     e.target.classList.remove("btn-red");
-      setCorrect(numberQuiz++)
-      setDisable(false);
+      e.target.classList.remove('btn-green');
+      e.target.classList.remove('btn-red');
+      setNumberQuiz(numberQuiz +1);
+      setDisableButtons(false);
+      setModalAcerto('hiddenModalAcerto');
+      setModalErro('hiddenModalErro');
     }
-
   }
   return (
     <div className="container">
+      <span className={modalAcerto}>
+        <ModalAcerto />
+      </span>
+      <span className={modalErro}>
+        <ModalErro />
+      </span>
+      <div className="jogar">
+      <h2>PONTUAÇÃO: {score}%</h2>
+        <p className="pergunta">{api[numberQuiz].question}</p>
+        <img
+          src={api[numberQuiz].imagem}
+          alt={api[numberQuiz].alt}
+          className="image-game"
+        />
+        <div className="jogar-jogo__btn">
+          <button
+            disabled={disableButtons}
+            className="btn-white"
+            key={1}
+            onClick={handleClick}
+          >
+            {api[numberQuiz].btns[0]}
+          </button>
+          <button
+            disabled={disableButtons}
+            className="btn-white"
+            key={2}
+            onClick={handleClick}
+          >
+            {api[numberQuiz].btns[1]}
+          </button>
+          <button
+            disabled={disableButtons}
+            className="btn-white"
+            key={3}
+            onClick={handleClick}
+          >
+            {api[numberQuiz].btns[2]}
+          </button>
 
-      <div className='jogar'>
-
-        <p className='pergunta'>{api[numberQuiz].question}</p>
-        <img src={api[numberQuiz].imagem} alt={api[numberQuiz].alt} className="image-game" />
-        <div className='jogar-jogo__btn'>
-          <button disabled={disable} className="btn-white" key={1} onClick={handleClick} >{api[numberQuiz].btns[0]}</button>
-          <button disabled={disable} className="btn-white" key={2} onClick={handleClick} >{api[numberQuiz].btns[1]}</button>
-          <button disabled={disable} className="btn-white" key={3} onClick={handleClick} >{api[numberQuiz].btns[2]}</button>
-
-          <button disabled={disable} className="btn-white" key={4} onClick={handleClick} >{api[numberQuiz].btns[3]}</button>
+          <button
+            disabled={disableButtons}
+            className="btn-white"
+            key={4}
+            onClick={handleClick}
+          >
+            {api[numberQuiz].btns[3]}
+          </button>
         </div>
       </div>
-
-    </div >
-  )
-}
-
+    </div>
+  );
+};
 
 export default TelaJogar;
