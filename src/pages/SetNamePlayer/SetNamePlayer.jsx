daeeb93e87c0bc0c64e2a74786eaca91e06d9fc7
Robert Argolo <robert.s.argolo@gmail.com>
2022-08-28 00:52:09 -0300

    tela setNamePlayer pronta

--- src/pages/SetNamePlayer/SetNamePlayer.jsx
+++ src/pages/SetNamePlayer/SetNamePlayer.jsx
@@ -1,39 +1,45 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import BackgroundContent from "../../components/BackgroundContentGame/BackgroundContentGame";
-import Button from "../../components/BtnStartLoad";
-import "./setNamePlayer.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import BackgroundContent from '../../components/BackgroundContentGame/BackgroundContentGame';
+import Button from '../../components/Button/Button';
+import './setNamePlayer.css';
 
 const SetNamePlayer = () => {
-  const [dados, setDados] = useState({ nome: '', idade: '' })
+  const [dados, setDados] = useState({ nome: '', idade: '' });
 
   const handleData = (e) => {
-    setDados({ ...dados, [e.target.name]: e.target.value })
-
-  }
+    setDados({ ...dados, [e.target.name]: e.target.value });
+  };
   function saveData() {
-    let users = []
-    localStorage.setItem('users', dados.nome.toUpperCase())
+    let users = [];
+    localStorage.setItem('users', dados.nome.toUpperCase());
     if (localStorage.getItem('users')) {
       users = JSON.parse(localStorage.getItem('users'));
-      const user = users.find(u => u.nome === u.nome);
+      const user = users.find((u) => u.nome === u.nome);
       if (user) {
         users.push(user);
-        localStorage.setItem('token', (dados.nome));
+        localStorage.setItem('token', dados.nome);
       }
     }
   }
 
   return (
     <BackgroundContent>
-          <form autoComplete="off" className="input-inicio">
-          <input className="input-inicio" type="text" name="nome" id="" placeholder="Digite seu nome aqui ..." onChange={handleData} value={dados.nome} />
-          <input  className="input-inicio" type="number" name="idade" id="" placeholder="Digite sua idade aqui ..." onChange={handleData} value={dados.idade}  />
-          </form>
-          <Link className='link' to={'/tutorial'}><Button click={saveData}>INICIAR JOGO</Button></Link>
-
+      <form autoComplete="off" className="form-inicio">
+        <input
+          className="input-name-player"
+          type="text"
+          name="nome"
+          placeholder="Digite seu nome aqui ..."
+          onChange={handleData}
+          value={dados.nome}
+        />
+      </form>
+      <Link className="link" to={'/tutorial'}>
+        <Button click={saveData}>INICIAR JOGO</Button>
+      </Link>
     </BackgroundContent>
-);
+  );
 };
 
 export default SetNamePlayer;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import TelaDeOpcao from "./pages/TelaDeOpcao";
 import Tutorial from './pages/Tutorial';
-import TelaDeInicio from './pages/TelaDeInicio';
 import TelaJogar from './pages/TelaJogar'
 import Home from "./pages/Home/Home";
 import SetNamePlayer from "./pages/SetNamePlayer/SetNamePlayer";
