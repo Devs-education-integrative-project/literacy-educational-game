4234563ee04a4330ff7aa1778b90f2fcc7eab911
Robert Argolo <robert.s.argolo@gmail.com>
2022-08-31 22:03:52 -0300

    responsividade tela tutorial pronta

--- src/Pages/Tutorial/Tutorial.js
+++ src/Pages/Tutorial/Tutorial.js
@@ -4,13 +4,14 @@ import BackgroundContent from '../../Components/BackgroundContentGame/Background
 import Button from '../../Components/Button/Button'
 import videoTutorial from '../../assets/tutorial.mp4'
 import './tutorial.css'
+import './responsive-tutorial.css'
 
 const Tutorial = () => {
 
   return (
     <BackgroundContent>
 
-      <video className='video-tutorial' title="Vídeo tutorial qual é a letra" width="320" height="240" controls><source src={videoTutorial} type="video/mp4" /></video>
+      <video className='video-tutorial' title="Vídeo tutorial qual é a letra" controls><source src={videoTutorial} type="video/mp4" /></video>
 
 
       <Link className='link' to={'/quiz'}><Button>PULAR TUTORIAL</Button></Link>
