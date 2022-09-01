2386162e39e3de4e0e4e051fbc3f3054cd91bb2d
Robert Argolo <robert.s.argolo@gmail.com>
2022-09-01 17:36:16 -0300

    arquivos main deletados

--- src/Components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import './button.css'
-
-const Button = ({ children, click,disableButton, styleButton}) => {
-  return (
-      <button disabled={disableButton} style={styleButton} className="btn" onClick={click}>{children}</button>
-  );
-}
-
-export default Button;
